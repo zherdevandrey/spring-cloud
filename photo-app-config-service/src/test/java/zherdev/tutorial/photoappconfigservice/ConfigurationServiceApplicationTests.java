cf5a0e4c0435382083a229da11ddcfc91826e9d1
mr.zherdev1992@gmail.com <mr.zherdev1992@gmail.com>
2020-04-14 00:06:48 +0300

    configuration service is added

---
+++ photo-app-config-service/src/test/java/zherdev/tutorial/photoappconfigservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zherdev.tutorial.photoappconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
